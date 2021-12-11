249564c590333ca688167b605d95df2198773d8a
Ethan Rodriguez <ethanrodriguez@Ethans-MacBook-Pro-2.local>
2021-12-11 10:26:24 -0800

    initial commit

---
+++ app/src/main/java/com/rodriguez/leavingthenest/ChatMessage.java
@@ -0,0 +1,36 @@
+package com.rodriguez.leavingthenest;
+public class ChatMessage {
+    private String author;
+    private String content;
+
+    public ChatMessage() {
+        author = "BLANK AUTHOR";
+        content = "BLANK CONTENT";
+    }
+
+    public ChatMessage(String author, String content) {
+        this.author = author;
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "From: " + author + "\n" + content;
+    }
+}
