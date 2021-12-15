57afdde06eeeeca05ccc1dea2bb61ac9610ba32b
Ethan Rodriguez <ethanrodriguez@Ethans-MacBook-Pro-2.local>
2021-12-14 22:23:41 -0800

    cleaning and laundry pages done

---
+++ app/src/main/java/com/rodriguez/leavingthenest/CookingActivity.java
@@ -0,0 +1,14 @@
+package com.rodriguez.leavingthenest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cooking);
+    }
+}
--- app/src/main/java/com/rodriguez/leavingthenest/ProductsActivity.java
+++ app/src/main/java/com/rodriguez/leavingthenest/ProductsActivity.java
@@ -1,14 +1,61 @@
 package com.rodriguez.leavingthenest;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ProductsActivity extends AppCompatActivity {
+    ActivityResultLauncher<Intent> launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+
+        ImageButton cleaningButton = findViewById(R.id.cleaningImageButton);
+        ImageButton laundryButton = findViewById(R.id.laundryImageButton);
+        ImageButton cookingButton = findViewById(R.id.cookingImageButton);
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                    }
+                });
+        cleaningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ProductsActivity.this, CleaningActivity.class);
+
+                launcher.launch(intent);
+            }
+        });
+        laundryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ProductsActivity.this, LaundryActivity.class);
+
+                launcher.launch(intent);
+            }
+        });
+        cookingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ProductsActivity.this, CookingActivity.class);
+
+                launcher.launch(intent);
+            }
+        });
+
     }
 }
